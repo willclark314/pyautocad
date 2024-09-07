e8964fdbeb97e8cb0a4c8da3608e464d4b9fec54
MyHome <will314@sohu.com>
2024-09-07 23:16:09 +0800

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -9,7 +9,7 @@ print(acad.doc.Name)
 p1 = APoint(0, 0)
 p2 = APoint(50, 25)
 for i in range(5):
-    text = acad.model.AddText('Hi %s!' % i, p1, 2.5)
+    text = acad.model.AddText(f'Hi {i}!', p1, 2.5)
     acad.model.AddLine(p1, p2)
     acad.model.AddCircle(p1, 10)
     p1.y += 10
